5528bb753bd2c3c7d85191ba9deb474483dae517
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-03 11:44:49 -0600

    Working on the state for the budget and using useMemo for validation

--- src/components/BudgetFrom.tsx
+++ src/components/BudgetFrom.tsx
@@ -1,4 +1,19 @@
+import { useMemo, useState } from "react"
+
 export default function BudgetForm() {
+    const [budget, setBudget] = useState(0);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+
+        setBudget(e.target.valueAsNumber)
+    }
+
+    const isValid = useMemo(() => {
+        return isNaN(budget) || budget <= 0
+    }, [budget])
+
+
     return (
         <form className="space-y-5">
             <div className="flex flex-col space-y-5">
@@ -11,11 +26,14 @@ export default function BudgetForm() {
                     className="w-full bg-white border-gray-200 p-2"
                     placeholder="Define tu presupuesto"
                     name="budget"
+                    value={budget}
+                    onChange={handleChange}
                 />
             </div>
             <input type="submit"
                 value='Definir presupuesto'
-                className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-bold uppercase"
+                className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-bold uppercase disabled:opacity-40"
+                disabled={isValid}
             />
         </form>
     )
