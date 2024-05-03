f7a0f1cad71a5d5e7e9a91d172fc552abf39237f
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-03 11:57:09 -0600

    Creating the reducer

---
+++ src/reducers/budgetReducer.ts
@@ -0,0 +1,24 @@
+export type BudgetActions = 
+|{ type: 'add-budget', payload: {budget: number}}
+
+export type BudgetState = {
+    budget: number
+}
+
+export const InitialState : BudgetState = {
+    budget: 0,
+}
+
+export const budgetReducer = (
+    state: BudgetState = InitialState,
+    action: BudgetActions
+) => {
+
+    if(action.type === 'add-budget'){
+        return {
+            ...state
+        }
+    }
+
+    return state
+}
