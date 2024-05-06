bc907347775861fc4b224cd9f5a3fc1596fe89b2
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-06 11:00:21 -0600

    Adding the functionality to change the value of the show argument of the modal

--- src/reducers/budgetReducer.ts
+++ src/reducers/budgetReducer.ts
@@ -1,25 +1,34 @@
-export type BudgetActions = 
-|{ type: 'add-budget', payload: {budget: number}}
+export type BudgetActions =
+  | { type: "add-budget"; payload: { budget: number } }
+  | { type: "show-modal" };
 
 export type BudgetState = {
-    budget: number
-}
+  budget: number;
+  modal: boolean;
+};
 
-export const InitialState : BudgetState = {
-    budget: 0,
-}
+export const InitialState: BudgetState = {
+  budget: 0,
+  modal: false,
+};
 
 export const budgetReducer = (
-    state: BudgetState = InitialState,
-    action: BudgetActions
+  state: BudgetState = InitialState,
+  action: BudgetActions
 ) => {
+  if (action.type === "add-budget") {
+    return {
+      ...state,
+      budget: action.payload.budget,
+    };
+  }
 
-    if(action.type === 'add-budget'){
-        return {
-            ...state,
-            budget: action.payload.budget
-        }
-    }
+  if (action.type === "show-modal") {
+    return {
+      ...state,
+      modal: true,
+    };
+  }
 
-    return state
-}
+  return state;
+};
