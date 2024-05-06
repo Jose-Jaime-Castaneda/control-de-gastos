a2b591500a3465576bfde92e539309000b7f4f5e
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-06 10:24:03 -0600

    Adding styles and content into the component BudgetTracker

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -4,8 +4,19 @@ export default function BudgetTracker() {
     const { state } = useBudget()
 
     return (
-        <>
-            <h2>Budget Tracker: {state.budget}</h2>
-        </>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+            <div className=" flex justify-center">
+                <img src="/grafico.jpg" alt="Grafica de gastos" />
+            </div>
+
+            <div className="flex flex-col justify-center items-center gap-8">
+                <button
+                    type="button"
+                    className=" bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg"
+                >
+                    Resetear App
+                </button>
+            </div>
+        </div>
     )
 }
