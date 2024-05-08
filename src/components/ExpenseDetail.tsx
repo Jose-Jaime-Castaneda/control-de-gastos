c9591e2496546731ab177ca12c94864ddb5813be
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-08 11:29:10 -0600

    Showing icons depending on the category of the expense

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,19 +1,27 @@
+import { useMemo } from "react"
 import { Expense } from "../types"
 import { formatDate } from "../utils"
 import AmountDisplay from "./AmoutDisplay"
+import { categories } from "../data/categories"
 
 type ExpenseDetailProps = {
     expense: Expense
 }
 
 export default function ExpenseDetail({ expense }: ExpenseDetailProps) {
+    const categoryInfo = useMemo(() => categories.filter(cat => cat.id === expense.category)[0], [expense])
+
     return (
         <div className=" bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center">
             <div>
-
+                <img src={`/icono_${categoryInfo.icon}.svg`}
+                    alt="Icono de categoria del gaasto"
+                    className="w-20"
+                />
             </div>
 
-            <div>
+            <div className=" flex-1 space-y-1">
+                <p className=" text-sm uppercase font-bold text-slate-500">{categoryInfo.name}</p>
                 <p>{expense.expenseName}</p>
                 <p className=" text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
             </div>
