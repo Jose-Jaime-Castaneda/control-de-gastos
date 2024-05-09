24627c05d4a47ab741275ea80beb645aafd2c2a3
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-09 12:14:45 -0600

    Adding a label tag and styles

--- src/components/FilterByCategory.tsx
+++ src/components/FilterByCategory.tsx
@@ -5,7 +5,8 @@ export default function FilterByCategory() {
         <div className=" bg-white shadow-lg rounded-lg p-10">
             <form>
                 <div className=" flex flex-col md:flex-row md:items-center gpa-5">
-                    <select name="" id="">
+                    <label htmlFor="category">Filtar Gastos</label>
+                    <select id="category" className=" bg-slate-100 p-3 rounded flex-1">
                         <option value={''}>-- Todas las cetegorias</option>
                         {categories.map(cat => (
                             <option key={cat.id} value={cat.name}>{cat.name}</option>
