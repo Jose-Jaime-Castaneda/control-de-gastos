0b0af08213b0246b48a5fcce12db21e17f22b478
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-09 12:30:20 -0600

    Now the filter is working

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -4,14 +4,14 @@ import ExpenseDetail from "./ExpenseDetail"
 
 export default function ExpenseList() {
     const { state } = useBudget()
-    const isEmpty = useMemo(() => state.expenses.length === 0, [state.expenses])
-
+    const filterExpense = state.currentCategory ? state.expenses.filter(exp => exp.category === state.currentCategory) : state.expenses
+    const isEmpty = useMemo(() => filterExpense.length === 0, [filterExpense])
     return (
         <div className="mt-10 bg-white shadow-lg rounded-lg p-5">
             {isEmpty ? <p className="text-gray-600 text-2xl font-bold">No hay gastos aún</p> : (
                 <>
                     <p className="text-gray-600 text-2xl font-bold my-5">Listado de gastos</p>
-                    {state.expenses.map(expense => (
+                    {filterExpense.map(expense => (
                         <ExpenseDetail
                             key={expense.id}
                             expense={expense}
